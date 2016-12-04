af0132ed134b0d8bb9400d7d08850d56afdc4175
avelar <fortunato.avelar@gmail.com>
2016-12-04 21:37:56 -0200

    improve styles

--- js/main.js
+++ js/main.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
   // $("body").fadeIn();
   $(".header-content").hide();
   $(".header-content").fadeIn(1000);
+
+  $(".book-scrool").draggable({
+    scroll:true,
+    start: function(){
+      $(this).data("startingScrollTop",$(this).parent().scrollTop());
+    },
+    drag: function(event,ui){
+      var st = parseInt($(this).data("startingScrollTop"));
+      ui.position.top -= $(this).parent().scrollTop() - st;
+    }
+  });
+
 });
