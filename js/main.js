ad6b0772ddf98ff1a8d06bf7abcbdc2ac7405127
avelar <fortunato.avelar@gmail.com>
2016-12-06 09:28:05 -0200

    improve microninteractions scrool

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 $(document).ready(function(){
-  // $("body").hide();
-  // $("body").fadeIn();
+  //microinteraction landing headline
   $(".header-content").hide();
   $(".header-content").fadeIn(1000);
 
-  $(".book-scrool").draggable({
-    scroll:true,
-    start: function(){
-      $(this).data("startingScrollTop",$(this).parent().scrollTop());
-    },
-    drag: function(event,ui){
-      var st = parseInt($(this).data("startingScrollTop"));
-      ui.position.top -= $(this).parent().scrollTop() - st;
+  //scroll to top show
+  $(document).scroll(function(){
+    if ($(this).scrollTop() > 400) {
+        $('.go-top').fadeIn();
+    } else {
+        $('.go-top').fadeOut();
     }
   });
+  //scroll to top action
+  $('.go-top').each(function(){
+      $(this).click(function(){
+          $('html,body').animate({ scrollTop: 0 }, 'slow');
+          return false;
+      });
+  });
 
+  $('.icon-arrow-down').each(function(){
+      $(this).click(function(){
+          $('html,body').animate({ scrollTop: 800 }, 'slow');
+          return false;
+      });
+  });
+
+  // $(".book-scrool").draggable({
+  //   scroll:true,
+  //   start: function(){
+  //     $(this).data("startingScrollTop",$(this).parent().scrollTop());
+  //   },
+  //   drag: function(event,ui){
+  //     var st = parseInt($(this).data("startingScrollTop"));
+  //     ui.position.top -= $(this).parent().scrollTop() - st;
+  //   }
+  // });
 });
