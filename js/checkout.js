bd43a937dba2ae5920bc702c0d50922457d6f091
avelar <fortunato.avelar@gmail.com>
2017-01-29 15:24:12 -0200

    improve ux payment steps

--- js/checkout.js
+++ js/checkout.js
@@ -4,28 +4,28 @@ $(document).ready(function(){
     $(".navegation-link-content").slideToggle();
   });
 
-  Iugu.setAccountID("COLOCAR SEU ACCOUNT ID AQUI");
-
-  jQuery(function($) {
-    $('#payment-form').submit(function(evt) {
-        var form = $(this);
-        var tokenResponseHandler = function(data) {
-
-            if (data.errors) {
-                // console.log(data.errors);
-                alert("Erro salvando cartão: " + JSON.stringify(data.errors));
-            } else {
-                $("#token").val( data.id );
-                form.get(0).submit();
-            }
-
-            // Seu código para continuar a submissão
-            // Ex: form.submit();
-        }
-
-        Iugu.createPaymentToken(this, tokenResponseHandler);
-        return false;
-    });
-  });
+  // Iugu.setAccountID("COLOCAR SEU ACCOUNT ID AQUI");
+  //
+  // jQuery(function($) {
+  //   $('#payment-form').submit(function(evt) {
+  //       var form = $(this);
+  //       var tokenResponseHandler = function(data) {
+  //
+  //           if (data.errors) {
+  //               // console.log(data.errors);
+  //               alert("Erro salvando cartão: " + JSON.stringify(data.errors));
+  //           } else {
+  //               $("#token").val( data.id );
+  //               form.get(0).submit();
+  //           }
+  //
+  //           // Seu código para continuar a submissão
+  //           // Ex: form.submit();
+  //       }
+  //
+  //       Iugu.createPaymentToken(this, tokenResponseHandler);
+  //       return false;
+  //   });
+  // });
 
 });//wrapper end
